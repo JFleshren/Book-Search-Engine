9ca59f4a064df3c32a687288de3899b633112a66
Fleshren <fleshren1@gmail.com>
2024-03-25 07:06:44 -0500

    Update dependencies and import statements

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,61 @@
+const { User } = require('../models');
+const { signToken, AuthenticationError } = require('../utils/auth');
+
+const resolvers = {
+  Query: {
+    me: async (parent, { userId }, context) => {
+      return User.findOne({ _id: userId });
+    },
+  },
+
+  Mutation: {
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+      if (!user) {
+        throw AuthenticationError;
+      }
+      const correctPw = await user.isCorrectPassword(password);
+      if (!correctPw) {
+        throw AuthenticationError;
+      }
+      const token = signToken(user);
+      return { token, user };
+    },
+
+    addUser: async (parent, { username, email, password }) => {
+      const user = await User.create({ username, email, password });
+      const token = signToken(user);
+      return { token, user };
+    },
+
+    saveBook: async (parent, { input }, context) => {
+      if (context.user) {
+        const user = await User.findByIdAndUpdate(
+          { _id: context.user._id },
+          { $push: { savedBooks: input } },
+          { new: true, runValidators: true }
+        );
+        return user;
+      }
+      throw AuthenticationError('You need to be logged in!');
+    },
+
+    removeBook: async (parent, { userId, bookId }, context) => {
+      if (context.user) {
+        return await User.findOneAndUpdate(
+          { _id: context.user._id },
+          {
+            $pull: {
+              savedBooks: {
+                bookId: bookId,
+              },
+            },
+          },
+          { new: true }
+        );
+      }
+    },
+  },
+};
+
+module.exports = resolvers;
